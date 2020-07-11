b9b48e2a9789d56cf9de84f89c66bcd7a06dacd4
Leon Coto <leoncoto@gmail.com>
2020-07-11 15:55:59 +0100

    Add fair and unfair coins

--- src/random.ts
+++ src/random.ts
@@ -1,5 +1,9 @@
-type CoinFlip = () => boolean;
+type Coin = () => boolean;
+type CoinFlip = (flip: Coin) => () => boolean;
+type UnfairCoin = Coin;
+type FairCoin = Coin;
 
-export const coinFlip: CoinFlip = () => {
-  return Math.random() < 0.5;
-};
+export const fairCoin: FairCoin = () => Math.random() < 0.5;
+export const headsCoin: UnfairCoin = () => false;
+export const tailsCoin: UnfairCoin = () => true;
+export const coinFlip: CoinFlip = (flip) => () => flip();
