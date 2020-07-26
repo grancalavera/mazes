ac6792b4dbdecf1eef83e7e43bc6dbee7f6c7fc6
momo <herr_moise@msn.com>
2020-07-26 18:25:20 +0100

    create the image

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,41 @@
-import * as g from "./grid";
-import * as sw from "./sidewinder";
-import * as r from "./random";
+import fs from "fs";
+import { PNG } from "pngjs";
 
-const coin = r.memoryCoin(true, 1);
-const sidewinder1 = sw.sidewinder(coin, r.chooseFirst);
-const sidewinder2 = sw.sidewinder(coin, r.chooseLast);
+let newfile = new PNG({ width: 10, height: 10 });
 
-import { maze1, maze2 } from "./sidewinder-fixtures";
+const toBinary = (x: boolean) => (x ? 1 : 0);
 
-console.log(g.print(maze1));
-console.log(g.print(sidewinder1([4, 4])));
-console.log("****");
-console.log(g.print(maze2));
-console.log(g.print(sidewinder2([4, 4])));
+console.log(newfile.width, newfile.height);
+
+for (let y = 0; y < newfile.height; y++) {
+  for (let x = 0; x < newfile.width; x++) {
+    let idx = (newfile.width * y + x) << 2;
+
+    const left = x < newfile.width >> 1;
+    const right = y < newfile.height >> 1;
+    const result = toBinary(left) ^ toBinary(right);
+    let col = result === 1 ? 0xe5 : 0xff;
+
+    if (y === 0 && x === 0) {
+      newfile.data[idx] = 255;
+      newfile.data[idx + 1] = 0;
+      newfile.data[idx + 2] = 0;
+    } else if (y === 0 && x === 1) {
+      newfile.data[idx] = 0;
+      newfile.data[idx + 1] = 0;
+      newfile.data[idx + 2] = 0;
+    } else {
+      newfile.data[idx] = x % 2 === 0 ? 255 : 0;
+      newfile.data[idx + 1] = 255;
+      newfile.data[idx + 2] = x % 2 === 0 ? 255 : 0;
+    }
+    newfile.data[idx + 3] = 0xff;
+  }
+}
+
+newfile
+  .pack()
+  .pipe(fs.createWriteStream(__dirname + "/newfile.png"))
+  .on("finish", function () {
+    console.log("Written!");
+  });
