7a3fd0d6c064df6893ba5035b045802817bc1433
Leon Coto <leoncoto@gmail.com>
2020-09-27 13:56:46 +0100

    get rid of yarn berry and add parcel

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,15 @@
-import fs from "fs";
-import { PNG } from "pngjs";
+import * as g from "./grid";
+import * as sw from "./sidewinder";
+import * as r from "./random";
 
-let newfile = new PNG({ width: 10, height: 10 });
+const coin = r.memoryCoin(true, 1);
+const sidewinder1 = sw.sidewinder(coin, r.chooseFirst);
+const sidewinder2 = sw.sidewinder(coin, r.chooseLast);
 
-const toBinary = (x: boolean) => (x ? 1 : 0);
+import { maze1, maze2 } from "./sidewinder-fixtures";
 
-console.log(newfile.width, newfile.height);
+console.log(g.print(maze1));
+console.log(g.print(sidewinder1([4, 4])));
 
-for (let y = 0; y < newfile.height; y++) {
-  for (let x = 0; x < newfile.width; x++) {
-    let idx = (newfile.width * y + x) << 2;
-
-    const left = x < newfile.width >> 1;
-    const right = y < newfile.height >> 1;
-    const result = toBinary(left) ^ toBinary(right);
-    let col = result === 1 ? 0xe5 : 0xff;
-
-    if (y === 0 && x === 0) {
-      newfile.data[idx] = 255;
-      newfile.data[idx + 1] = 0;
-      newfile.data[idx + 2] = 0;
-    } else if (y === 0 && x === 1) {
-      newfile.data[idx] = 0;
-      newfile.data[idx + 1] = 0;
-      newfile.data[idx + 2] = 0;
-    } else {
-      newfile.data[idx] = x % 2 === 0 ? 255 : 0;
-      newfile.data[idx + 1] = 255;
-      newfile.data[idx + 2] = x % 2 === 0 ? 255 : 0;
-    }
-    newfile.data[idx + 3] = 0xff;
-  }
-}
-
-newfile
-  .pack()
-  .pipe(fs.createWriteStream(__dirname + "/newfile.png"))
-  .on("finish", function () {
-    console.log("Written!");
-  });
+console.log(g.print(maze2));
+console.log(g.print(sidewinder2([4, 4])));
