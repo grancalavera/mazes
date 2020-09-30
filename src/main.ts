f907143c15539320301a00b80e86edb2e2b488e1
Leon Coto <leoncoto@gmail.com>
2020-09-30 23:06:42 +0100

    render four by four example

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { distances } from "./grid";
 import * as render from "./render";
 import { maze1 } from "./sidewinder-fixtures";
+import "./render-four-of-each";
 
 render.toConsole(maze1);
 console.log(JSON.stringify(distances(maze1, [0, 0]), null, 2));
